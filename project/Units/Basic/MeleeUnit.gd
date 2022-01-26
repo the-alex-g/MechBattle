e4cff27546fce663d6fcf99e7d9d6bba0cfaedf0
Alex Gestwicki <alex.gestwicki@gmail.com>
2022-01-26 11:13:20 -0500

    Make behavior work with four units
    It's a two v two deal. They chase each other, fight, one survives and starts wandering.

--- project/Units/Basic/MeleeUnit.gd
+++ project/Units/Basic/MeleeUnit.gd
@@ -15,40 +15,43 @@ var can_hit := false
 var is_cooling_down := false
 
 # onready variables
+onready var _cooldown_timer := $AttackDelayTimer
 
 
 func _process(_delta:float)->void:
-	if movement_state == MOVEMENT_STATE.DEAD:
+	if is_dead: # if dead, do nothing
 		return
+	
 	if can_hit and not is_cooling_down:
 		var damage_dealt = damage
 		if effective_attack_against.has(target.unit_name.to_upper()):
 			damage_dealt *= 2
 		target.hit(damage_dealt, unit_name)
 		is_cooling_down = true
-		$AttackDelayTimer.start(swing_delay)
+		_cooldown_timer.start(swing_delay)
+	
 	if movement_state == MOVEMENT_STATE.WANDER:
-		$AttackDelayTimer.stop()
+		_cooldown_timer.stop()
 		can_hit = false
 		is_cooling_down = false
 
 
 func _on_HitArea_body_entered(body:Node)->void:
-	if movement_state != MOVEMENT_STATE.DEAD:
-		if body == target:
+		if body == target and not is_dead:
 			can_hit = true
 			movement_state = MOVEMENT_STATE.STATIONARY
 
 
 func _on_HitArea_body_exited(body:Node)->void:
-	if movement_state != MOVEMENT_STATE.DEAD:
-		if body == target:
+		if body == target and not is_dead:
 			can_hit = false
 			movement_state = MOVEMENT_STATE.CHASE
 
 
 func _on_AttackDelayTimer_timeout()->void:
-	if movement_state == MOVEMENT_STATE.DEAD:
-		$AttackDelayTimer.stop()
+	if is_dead:
+		_cooldown_timer.stop()
+		# if you're dead, stop the timer
 	else:
+		# otherwise, you can hit again
 		is_cooling_down = false
--- project/Units/Basic/Unit.gd
+++ project/Units/Basic/Unit.gd
@@ -5,13 +5,14 @@ extends KinematicBody
 signal dead
 
 # enums
-enum MOVEMENT_STATE {WANDER, CHASE, STATIONARY, DEAD}
+enum MOVEMENT_STATE {WANDER, CHASE, STATIONARY}
 
 # constants
 
 # exported variables
 export var team_id := "GOOD"
 export var unit_name := ""
+export var id := ""
 export var effective_attack_against := [] # all uppercase
 export var effective_defense_against := [] # all uppercase
 export var speed := 1
@@ -20,31 +21,41 @@ export var health := 10
 # variables
 var _ignore
 var movement_state = MOVEMENT_STATE.WANDER
-var potential_targets := []
 var target:KinematicBody = null
 var _wander_timer_not_started := true
 var _is_first_run := true
+var is_dead := false
 
 # onready variables
 onready var sight_range := $Range
+onready var wander_timer := $DirectionChangeTimer
 
 
 func _physics_process(delta:float)->void:
-	if movement_state == MOVEMENT_STATE.DEAD or movement_state == MOVEMENT_STATE.STATIONARY:
+	if is_dead or movement_state == MOVEMENT_STATE.STATIONARY:
 		return
-	var velocity = Vector2.ZERO
+	
+	var velocity = Vector2.ZERO # velocity is a public variable because it needs to be accesable from
+	# classes that inherit this one.
+	
 	if movement_state == MOVEMENT_STATE.CHASE and target != null:
-		var target_position:Vector3 = target.get_global_transform().origin
-		var vector := target_position-get_global_transform().origin
+		
+		var target_position := target.get_global_transform().origin
+		var position_difference := target_position - get_global_transform().origin
+		
 		look_at(target_position, Vector3.UP)
-		rotation.y += 0.5*TAU
-		vector = vector.normalized()
-		velocity = Vector2(vector.x, vector.z)
+		rotation.y += TAU * 0.5
+		position_difference = position_difference.normalized()
+		velocity = Vector2(position_difference.x, position_difference.z)
+		
 	elif movement_state == MOVEMENT_STATE.WANDER:
 		if _wander_timer_not_started:
-			print("OVERRIDE")
-			$DirectionChangeTimer.start()
+			
+			print(id + " start wandering")
+			
+			wander_timer.start()
 			_wander_timer_not_started = false
+		
 		velocity = Vector2.LEFT
 		velocity = velocity.rotated(rotation.y)
 	velocity *= delta*speed
@@ -53,33 +64,51 @@ func _physics_process(delta:float)->void:
 
 func _on_Range_body_entered(body:Node)->void:
 	if body.is_in_group("UNIT"):
-		if body.team_id != team_id:
-			potential_targets.append(body)
-			if target == null:
+		if body.team_id != team_id: # if the target is on a different team
+			
+			if target == null: # if you don't already have a target
+				print(id + " target aquired: " + body.id)
 				target = body
 				_ignore = body.connect("dead", self, "_on_target_dead")
 
 
 func _on_target_dead()->void:
+	print(id + " target defeated: " + target.id)
+	
 	target.disconnect("dead", self, "_on_target_dead")
-	potential_targets.erase(target)
-	if potential_targets.size() > 0:
-		target = potential_targets[randi()%potential_targets.size()]
+	
+	var overlapping_bodies:Array = sight_range.get_overlapping_bodies()
+	
+	if overlapping_bodies.size() > 0:
+		var potential_targets := []
+		
+		for body in overlapping_bodies:
+			if body.is_in_group("UNIT"):
+				if body.team_id != team_id and not body.is_dead:
+					potential_targets.append(body)
+		
+		if potential_targets.size() > 0:
+			var new_target_index = randi() % potential_targets.size()
+			target = potential_targets[new_target_index]
+			_ignore = target.connect("dead", self, "_on_target_dead")
+			print(_ignore)
+		
+			print(id + " new target aquired: " + target.id)
+	
+		else:
+			target = null
+			movement_state = MOVEMENT_STATE.WANDER
+	
 	else:
 		target = null
 		movement_state = MOVEMENT_STATE.WANDER
-		_wander_timer_not_started = true
-
-
-func _on_Range_body_exited(body:Node)->void:
-	if potential_targets.has(body):
-		potential_targets.erase(body)
 
 
 func _on_DirectionChangeTimer_timeout()->void:
 	if target != null:
 		movement_state = MOVEMENT_STATE.CHASE
-		$DirectionChangeTimer.stop()
+		wander_timer.stop()
+		_wander_timer_not_started = true
 	else:
 		rotation.y = randf()*TAU
 
@@ -90,4 +119,6 @@ func hit(damage_taken:int, name_of_unit:String)->void:
 	health -= damage_taken
 	if health <= 0:
 		emit_signal("dead")
-		movement_state = MOVEMENT_STATE.DEAD
+		is_dead = true
+		
+		print(id + " is dead")
