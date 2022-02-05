d4ed68675da0894829f5175fab0abcea8df6ff05
the-alex-g <alex.gestwicki@gmail.com>
2022-02-05 17:32:21 -0500

    Add seperate tabs for different categories of units

--- project/UI/HUD.gd
+++ project/UI/HUD.gd
@@ -7,12 +7,14 @@ const _GOOD_UNIT_NAMES := [
 	"Ranged Unit",
 ]
 const _BAD_UNIT_NAMES := [
-	
+	"Bad Unit",
 ]
-const _GOOD_UNIT_PATHS := {
+const _UNIT_PATHS := {
 	"Melee Unit":"res://Units/Basic/MeleeUnit.tscn",
 	"Ranged Unit":"res://Units/Basic/MeleeUnit.tscn",
+	"Bad Unit":"res://Units/Basic/MeleeUnit.tscn",
 }
+const _UNIT_SETS := 2
 
 enum UnitSet {GOOD, BAD}
 
@@ -20,30 +22,35 @@ export var button_height := 24.0
 
 var _unit_set = UnitSet.GOOD
 
-onready var _button_container = $HBoxContainer as HBoxContainer
+onready var _tabs = $TabContainer as TabContainer
 
 func _ready()->void:
-	_button_container.margin_top = -button_height
+	for unit_set in _UNIT_SETS:
+		var button_container := HBoxContainer.new()
+		button_container.name = UnitSet.keys()[unit_set]
+		button_container.margin_top = -button_height
 	
-	var _unit_names := []
-	match _unit_set:
-		UnitSet.GOOD:
-			_unit_names = _GOOD_UNIT_NAMES
-		UnitSet.BAD:
-			_unit_names = _BAD_UNIT_NAMES
+		var unit_names := []
+		print(unit_set)
+		match unit_set:
+			UnitSet.GOOD:
+				unit_names = _GOOD_UNIT_NAMES
+			UnitSet.BAD:
+				unit_names = _BAD_UNIT_NAMES
+
+		assert(unit_names.size() > 0)
 	
-	assert(_unit_names.size() > 0)
-	
-	var UnitButton := load("res://UI/UnitButton.tscn")
-	for unit_name in _unit_names:
-		var unit_button = UnitButton.instance() as Button
-		_button_container.add_child(unit_button)
-		unit_button.unit_name = unit_name
-		# warning-ignore:return_value_discarded
-		unit_button.connect("custom_pressed", self, "_on_UnitButton_pressed")
+		var UnitButton := load("res://UI/UnitButton.tscn")
+		for unit_name in unit_names:
+			var unit_button = UnitButton.instance() as Button
+			button_container.add_child(unit_button)
+			unit_button.unit_name = unit_name
+			# warning-ignore:return_value_discarded
+			unit_button.connect("custom_pressed", self, "_on_UnitButton_pressed")
+		_tabs.add_child(button_container)
 
 
 func _on_UnitButton_pressed(unit_name:String)->void:
-	var unit_path = _GOOD_UNIT_PATHS[unit_name] as String
+	var unit_path = _UNIT_PATHS[unit_name] as String
 	emit_signal("unit_selected", unit_path)
 
